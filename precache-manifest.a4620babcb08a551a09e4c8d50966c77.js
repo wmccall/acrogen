342998a759d76c67c54b4535e4d4dba52c2714cb
William McCall <wnm.code@gmail.com>
2020-05-04 22:53:24 -0400

    Updates

---
+++ precache-manifest.a4620babcb08a551a09e4c8d50966c77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f399872100c50c3e76208951e40e8ea1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7706b73c24edebc192dc",
+    "url": "/static/css/main.c29b0bf3.chunk.css"
+  },
+  {
+    "revision": "179f7c97fdae20a04137",
+    "url": "/static/js/2.0c54ca6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0c54ca6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7706b73c24edebc192dc",
+    "url": "/static/js/main.90619c14.chunk.js"
+  },
+  {
+    "revision": "ba19880f10da417304c7",
+    "url": "/static/js/runtime-main.42970c21.js"
+  }
+]);
